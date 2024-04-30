2aa7a0617133791a80657084968def24258e4a98
devphpru <devphpru@yandex.ru>
2024-04-30 15:08:25 +0300

    dev 9 fix (Правки тестов)

--- app/Http/Requests/AdminUserRequest.php
+++ app/Http/Requests/AdminUserRequest.php
@@ -16,9 +16,9 @@ class AdminUserRequest extends FormRequest
         $id = $this->admin_user->id ?? null;
 
         return [
-            'username' => 'required|string|min:2|max:255|regex:/^[А-ЯЁёа-яA-Za-z ]+$/u',
-            'email' => 'required|string|max:1000|email|unique:admin_users,email,' . $id,
-            'password' => ($id ? 'nullable' : 'required') . '|string|min:5|max:255|confirmed|regex:/^[A-Za-z0-9]+$/u',
+            'username' => 'bail|required|string|min:2|max:255|regex:/^[А-ЯЁёа-яA-Za-z ]+$/u',
+            'email' => 'bail|required|string|max:1000|email|unique:admin_users,email,' . $id,
+            'password' => ($id ? 'bail|nullable' : 'bail|required') . '|string|min:5|max:255|confirmed|regex:/^[A-Za-z0-9]+$/u',
         ];
     }
 
